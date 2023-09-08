a1617766d910c3171a5faf381c8279455c634ed7
v-wajian <v-wajian@andy94wi11001>
2023-09-07 22:47:39 -0700

    Add project files.

---
+++ EmailSender/Models/Email.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmailSender.Models
+{
+    public class Email
+    {
+        [Required]
+        [EmailAddress]
+        public string To { get; set; }
+        public string From { get; set; }
+        [Required]
+        [StringLength(4)]
+        public string Subject { get; set; }
+        [Required]
+        public string Text { get; set; }
+    }
+}
